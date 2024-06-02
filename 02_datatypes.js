7bc9410ac460c3f632a5d0b3d27dbaa86323d686
Vansh Verma <106549024+vansh401@users.noreply.github.com>
2024-06-02 23:20:48 +0530

    1

---
+++ 02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //---->> treat all JS code as newer versions
+
+// alert("hello"); --->> we are using a node.js not a browser  
+
+
+// datatypes-->
+// Number
+// BigInt
+// String =""
+// Boolean = true/false
+// null= standalone value
+// undefined
+// Symbol= unique 
+// object
+
+console.log(typeof "vansh");  //---> string
+console.log(typeof undefined); //---> undefined
+console.log(typeof null); //---> object   ******
