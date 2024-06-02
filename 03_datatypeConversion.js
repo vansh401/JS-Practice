6e205f8740fc1ce9908b2e1decaa22f5e177d80b
Vansh Verma <106549024+vansh401@users.noreply.github.com>
2024-06-02 23:29:42 +0530

    1

---
+++ 03_datatypeConversion.js
@@ -0,0 +1,13 @@
+let score="33";
+let score1="33anc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+let valueInNumber2 = Number(score1);
+
+// console.log(typeof valueInNumber);
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+// console.log(typeof NaN);
